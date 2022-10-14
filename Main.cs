2d8e9f5808383cabf9adb0bd7e9a129a11335de5
MichaelGallinago <67271938+MichaelGallinago@users.noreply.github.com>
2022-10-14 20:55:06 +0500

    Cringe

--- Main.cs
+++ Main.cs
@@ -5,15 +5,15 @@ namespace FP2Dualsense.MichaelGallinago
 {
     public static class BuildInfo
     {
-        public const string Name = "FP2Rebalance"; // Name of the Mod.  (MUST BE SET)
+        public const string Name = "FP2Dualsense"; // Name of the Mod.  (MUST BE SET)
 
         public const string Description =
-            "Mod that changes the balance of the game."; // Description for the Mod.  (Set as null if none)
+            "Mod that adds support for dualsense functions."; // Description for the Mod.  (Set as null if none)
 
         public const string Author = "Michael Gallinago"; // Author of the Mod.  (MUST BE SET)
         public const string Company = null; // Company that made the Mod.  (Set as null if none)
-        public const string Version = "1.1.5.0"; // Version of the Mod.  (MUST BE SET)
-        public const string DownloadLink = "https://github.com/MichaelGallinago/FreedomPlanet2Rebalance"; // Download Link for the Mod.  (Set as null if none)
+        public const string Version = "1.6.0"; // Version of the Mod.  (MUST BE SET)
+        public const string DownloadLink = "https://github.com/MichaelGallinago/FP2Dualsense"; // Download Link for the Mod.  (Set as null if none)
     }
 
     public class FP2Dualsense : MelonMod
@@ -23,7 +23,7 @@ namespace FP2Dualsense.MichaelGallinago
         [System.Obsolete]
         public override void OnApplicationStart() // Runs after Game Initialization.
         {
-            MelonLogger.Msg("FP2Rebalance is active.");
+            MelonLogger.Msg("FP2Dualsense is active.");
             MelonPreferences.Load();
             MelonPreferences.Save();
         }
@@ -35,7 +35,7 @@ namespace FP2Dualsense.MichaelGallinago
             {
                 goFP2Dualsense = new GameObject();
                 GameObject.DontDestroyOnLoad(goFP2Dualsense);
-                var mtc = goFP2Dualsense.AddComponent<FP2DualsenseBehaviour>();
+                var mtc = goFP2Dualsense.AddComponent<DSSupport>();
             }
         }
         */
